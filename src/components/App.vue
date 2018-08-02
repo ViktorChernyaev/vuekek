a4e4581a7164c1f2159b9d416d8dded06d13f215
ViktorChernyaev <bagunok18@gmail.com>
2018-08-02 22:46:43 +0300

    Init

---
+++ src/components/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>Hello, app</div>
+</template>
+
+<script>
+    export default {
+        name: "app"
+    }
+</script>
+
+<!-- CSS libraries -->
+<style src="normalize.css/normalize.css"></style>
+
+<!-- Global CSS -->
+<style>
+    body {
+        margin: 0;
+    }
+</style>
